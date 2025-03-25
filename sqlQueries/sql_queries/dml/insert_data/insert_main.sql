c303ad861e16d709aaf1dd9f089467dfd6ab989b
namithaakanumuru <kanumuru.na@northeastern.edu>
2025-03-24 21:45:48 -0400

    added insert queries

---
+++ sqlQueries/sql_queries/dml/insert_data/insert_main.sql
@@ -0,0 +1,9 @@
+@insert_location.sql
+@insert_data_person_type.sql
+@insert_data_blood_type.sql
+@insert_hospital.sql
+@insert_blood_bank.sql
+@insert_bbms_user.sql
+@insert_hospital_patient.sql
+@insert_data_blood_request.sql
+@insert_donation.sql
